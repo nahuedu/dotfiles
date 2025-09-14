0d666935fa683862eb8d8f83cfb79d33cc784b5a
Nahuel Andreoli <andreoli.nahuel@gmail.com>
2025-09-14 20:06:32 -0300

    details

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ mkdir -p $HOME/.config/omp
 cp -ivr omp/nano.omp.json $HOME/.config/omp
 
 HOMEBREW_NO_INSTALL_UPGRADE=true brew install bat fzf fd jq yq jo jandedobbeleer/oh-my-posh/oh-my-posh tmux git
+
+HOMEBREW_NO_INSTALL_UPGRADE=true brew install --cask font-jetbrains-mono
