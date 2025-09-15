9f7440fed3ef152dc0d1c3880c39fbeb6a7475ee
Nahuel Andreoli <andreoli.nahuel@gmail.com>
2025-09-15 18:15:42 -0300

    fisable brew update on install

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ cp -ivr ghostty/config $HOME/.config/ghostty
 mkdir -p $HOME/.config/omp
 cp -ivr omp/nano.omp.json $HOME/.config/omp
 
-HOMEBREW_NO_INSTALL_UPGRADE=true brew install bat fzf fd jq yq jo jandedobbeleer/oh-my-posh/oh-my-posh tmux git
+HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_NO_INSTALL_UPGRADE=true brew install bat fzf fd jq yq jo jandedobbeleer/oh-my-posh/oh-my-posh tmux git
 
-HOMEBREW_NO_INSTALL_UPGRADE=true brew install --cask font-jetbrains-mono
+HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_NO_INSTALL_UPGRADE=true brew install --cask font-jetbrains-mono
