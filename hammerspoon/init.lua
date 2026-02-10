265782c3f61d95cf0f903d659de8470cf7024d16
Nahuel Andreoli <andreoli.nahuel@gmail.com>
2026-02-10 19:00:53 -0300

    added hammerspoon

---
+++ hammerspoon/init.lua
@@ -0,0 +1,43 @@
+--[[
+hs.hotkey.bind({"alt"}, "space", function()
+
+    ch = hs.chooser.new(function(selected)
+        hs.urlevent.openURL(selected.subText)
+
+        end)
+    ch:bgDark(true)
+    ch:choices({{
+        text = "youtube",
+        subText = "https://www.youtube.com"
+        }})
+
+
+    ch:show()
+end)
+]]
+
+hs.hotkey.bind({"alt", "ctrl"}, "Left", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"alt", "ctrl"}, "Right", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
--- install.sh
+++ install.sh
@@ -12,10 +12,13 @@ cp -ivr vim/vimrc $HOME/.config/vim
 mkdir -p $HOME/.config/tmux
 cp -ivr tmux/tmux.conf $HOME/.config/tmux
 
+mkdir -p $HOME/.hammerspoon
+cp -ivr hammerspoon/init.lua $HOME/.hammerspoon
+
 mkdir -p $HOME/.config/intellij
 cp -ivr intellij/intellij $HOME/.config/intellij
 chmod +x $HOME/.config/intellij/intellij
 
 HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_NO_INSTALL_UPGRADE=true brew install bat fzf fd jq yq jo jandedobbeleer/oh-my-posh/oh-my-posh tmux git
 
-HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_NO_INSTALL_UPGRADE=true brew install --cask font-jetbrains-mono
+HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_NO_INSTALL_UPGRADE=true brew install --cask font-jetbrains-mono maccy karabiner-elements hammerspoon finetune
